b3c665600250ca72f4100324d517d96da434b37d
Sainabs <Sainab.sahal1@gmail.com>
2022-09-30 12:09:10 -0500

    eda

--- Insurance_access.py
+++ Insurance_access.py
@@ -6,3 +6,13 @@ import matplotlib.pyplot as plt
 
 title = st.title("Insurance Access Dashboard")
 df = pd.read_csv(".csv")
+
+
+
+
+
+
+
+ch = st.checkbox('I understand this data')
+if ch:
+    st.write("Thanks for visiting Sainab's Admin Dashboard")
